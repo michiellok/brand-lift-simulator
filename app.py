ed7615a4d29c89a92bfc9c2917b6644b63aaf75b
michiellok <michiel.lok@gmail.com>
2025-02-02 09:22:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -92,6 +92,7 @@ with tab4:
     st.header("📂 Download Resultaten")
     df_export = pd.DataFrame({"Kanaal": list(st.session_state["media_alloc"].keys()), "Huidige Allocatie": list(st.session_state["media_alloc"].values()), "Brand Lift": list(brand_lift_per_channel.values())})
     csv = df_export.to_csv(index=False).encode('utf-8')
-    st.download_button("📥 Download als CSV 🛈", help="Download de resultaten van de analyse als een CSV-bestand voor verdere verwerking."), data=csv, file_name="brand_lift_results.csv", mime='text/csv')
+    st.download_button("📥 Download als CSV 🛈", help="Download de resultaten van de analyse als een CSV-bestand voor verdere verwerking.", data=csv, file_name="brand_lift_results.csv", mime='text/csv')
+
 
 
